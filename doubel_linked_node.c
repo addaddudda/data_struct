bf01e17c40701109024691a069d9cf0919298b58
addaddudda <hwon3330@naver.com>
2026-08-26 10:19:22 -0600

    포인터 써서 인덱싱까지 완료

---
+++ doubel_linked_node.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+Node *head, *tail;
+
+
+int index_node(int order){
+    Node* node = head -> next;
+    for(int i = 0; i < order - 1; i++){
+        node = node -> next;
+    }
+    return node -> data;
+}
+
+
+
+void insert(int data){
+    Node *node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    Node* cur;
+    cur = tail -> prev;
+
+    cur -> next = node;
+    node -> prev = cur;
+
+    node -> next = tail;
+    tail -> prev = node;
+}
+
+void delete(int order){
+    Node* node = head -> next;
+    for(int i = 0; i < order - 1; i++){
+        node = node -> next;
+    }
+    node -> prev -> next = node -> next;
+    node -> next -> prev = node -> prev;
+
+    free(node);
+}
+
+
+
+
+void show(){
+    Node* cur = head -> next;
+    while(cur != tail){
+        printf("%d ", cur -> data);
+        cur = cur -> next;
+    }
+}
+
+
+
+int main(void){
+    head = (Node*)malloc(sizeof(Node));
+    tail = (Node*)malloc(sizeof(Node));
+
+    head->next = tail;
+    head->prev = head;
+
+    tail->next = tail;
+    tail->prev = head;
+
+    insert(1);
+    insert(3);
+    insert(100);
+
+
+    delete(2);
+
+    printf("%d\n", index_node(1));
+    
+    show();
+
+    
+}
